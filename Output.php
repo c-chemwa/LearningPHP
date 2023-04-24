e5e0915dac92a8512c486d63609552cbbee1eef0
c-chemwa <calebchemwa@gmail.com>
2023-04-24 10:09:53 +0300

    Learning variables in php

---
+++ Output.php
@@ -0,0 +1,27 @@
+<?php
+   
+
+   //Single line comment
+
+   /*Multiline
+   comment
+
+   */
+  //echo can print more than one output(html, string, numbers, etc)
+  //echo 'hi',123;
+
+  //print can only print once(one arguement)
+
+  //print 'hey';
+
+  //print_r() prints single values and arrays
+  //print_r([1,2,3]);
+
+  //var_dump() is used to return more information like data type and length
+  //var_dump('Hello');
+
+  //var_export() it gives a string representation of a variable
+  //var_export('Hello');
+?>
+
+
---
+++ Variables.php
@@ -0,0 +1,23 @@
+<?php
+    /*
+    String
+    Integer
+    float
+    boolean
+    arrays
+    objects
+    null
+    resource
+    */
+    /*
+    There are rules to naming variables in php
+    1. Prefixed with '$'
+    2. Must start with a letter/underscore
+    3. Can only have alphanumeric character
+    4. Case-sensitive
+    */
+
+    //String
+    $Name = 'Caleb';
+    echo $Name;
+?>
